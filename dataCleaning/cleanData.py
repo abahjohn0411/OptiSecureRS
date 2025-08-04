e150250eaf0796074e5d5fc19cbd1be33beb28d5
John A Abah <joabah@yahoo.co.uk>
2025-08-04 19:52:38 +0100

    preprocessing pipeline

--- dataCleaning/cleanData.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-
-df = pd.read_csv(r'C:\Users\ASUS PC\Desktop\AMDARI INTERNSHIP\optiSecure\OptiSecure_RS\dataCleaning\df.csv', parse_dates=['InteractionDate'])
-print(df.head())
---
+++ dataCleaning/preprocessing.py
@@ -0,0 +1,49 @@
+# Import necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+# Create a file path
+file_path = r'C:\Users\ASUS PC\Desktop\AMDARI INTERNSHIP\optiSecure\OptiSecure_RS\dataCleaning\df.csv'
+
+def preprocessing(file_path):
+    '''Data Preprocessing Script'''
+    
+    # Load dataset
+    df = pd.read_csv(file_path, 
+                     parse_dates=['IssuanceDate', 'ExpiryDate', 'ClaimDate', 'InteractionDate', 'ResponseTime', 'RenewalDate'])
+
+    # Identify numeric and categorical columns
+    num_col = df.select_dtypes(include=[np.number]).columns
+    cat_col = df.select_dtypes(include=['object']).columns  
+
+    # Handle missing values
+    df[num_col] = df[num_col].fillna(df[num_col].mean())
+    for col in cat_col:
+        df[col] = df[col].fillna(df[col].mode()[0])
+
+    # Handle outliers using IQR method
+    for col in num_col:
+        q1 = df[col].quantile(0.25)
+        q3 = df[col].quantile(0.75)
+        iqr = q3 - q1
+        lower_bound = q1 - 1.5 * iqr
+        upper_bound = q3 + 1.5 * iqr
+        df[col] = np.where((df[col] < lower_bound) | (df[col] > upper_bound), 0, df[col])
+
+    # Clean and standardize text columns
+    for col in cat_col:
+        df[col] = df[col].astype(str).str.strip().str.lower()  # trim + lowercase
+        df[col] = df[col].astype('category')
+
+    # Scale selected numeric columns
+    scaler = StandardScaler()
+    df_scale = ['Income', 'Premium']
+    
+    df[df_scale] = scaler.fit_transform(df[df_scale])
+
+    return df
+
+
+    
+
