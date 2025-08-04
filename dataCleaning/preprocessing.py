7cfd9206d1b6b7d872a6aa71395faffcbee00a02
John A Abah <joabah@yahoo.co.uk>
2025-08-04 21:42:56 +0100

    new_

--- dataCleaning/preprocessing.py
+++ dataCleaning/preprocessing.py
@@ -43,8 +43,13 @@ def preprocessing(file_path):
 
     # Confirm completion
     print(f"✅ Data preprocessing completed successfully. Data shape: Rows: {df.shape[0]}, Columns: {df.shape[1]}")
+    print(df.describe(include=(np.number)))
 
     return df
+
+if __name__ == "__main__":
+    df_cleaned = preprocessing(file_path)
+
 
 
 
